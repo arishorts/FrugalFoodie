a84ebc84fa129ebbe269fbe613c1b7fd74964f67
arishorts <arieljschwartz@gmail.com>
2023-02-07 20:21:42 -0700

    Created functions that pull Spoonacular API to search for grocery products or by ingredient. It will also validate the user input. Then it displays cards on the screen dynamically using bootstrap. This will be updated for use with Tailwind. Additionally, includes the Google Places API that displays a proof of concept

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,3 @@
-//API = AIzaSyAYNCDLPp5lDjmVzQk0Q3T3xDoqNyjVllY
-//'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos.coords.longitude}%2C${pos.coords.latitude}&radius=1500&type=market&key=AIzaSyAYNCDLPp5lDjmVzQk0Q3T3xDoqNyjVllY',
-// let map = null;
-
-<script
-  async
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAYNCDLPp5lDjmVzQk0Q3T3xDoqNyjVllY&libraries=places&callback=initMap"
-></script>;
-
 // function initMap() {
 //   navigator.geolocation.getCurrentPosition((pos) => {
 //     // map = new google.maps.Map(document.getElementById("map"), {
@@ -71,7 +62,7 @@ function createMarker(place) {
   });
 }
 
-// window.initMap = initMap;
+window.initMap = initMap;
 
 function searchNearMe(lat, long, search) {
   var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${long}%2C${lat}&radius=1500&type=${search}&key=AIzaSyAYNCDLPp5lDjmVzQk0Q3T3xDoqNyjVllY`;
@@ -82,4 +73,136 @@ function searchNearMe(lat, long, search) {
     });
 }
 
-// initMap();
+const spoonacularApp = {
+  //initiate app
+  init: () => {
+    $("#byIngredientsForm").submit((event) => {
+      event.preventDefault();
+      spoonacularApp.validateByIngredients();
+    });
+    $("#searchGroceryProductForm").submit((event) => {
+      event.preventDefault();
+      spoonacularApp.validateGroceryProduct();
+    });
+  },
+
+  apiCall: (userRequest, queries, options) => {
+    const apikey = "?apiKey=34d81d44cd7b469c9a2f5d3f458d078c";
+    var url = `https://api.spoonacular.com/${userRequest}${apikey}${queries}`;
+    //console.log(url);
+    return fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        spoonacularApp.success(data);
+        return data; //why isnt this returning when i set a variable
+      })
+      .catch((error) => {
+        spoonacularApp.fail(error);
+      });
+  },
+
+  success: (data) => {
+    console.log(data);
+  },
+
+  fail: (error) => {
+    console.log(error);
+    console.log("you have an error");
+  },
+
+  validateByIngredients: () => {
+    var recipeArray = $("#byIngredientsInput").val().split(",");
+    var recipeString = recipeArray[0];
+    for (let index = 1; index < recipeArray.length; index++) {
+      recipeString = recipeString + ",+" + recipeArray[index].trim();
+    }
+    //console.log(recipeString);
+    spoonacularApp.searchByIngredient(recipeString);
+  },
+
+  searchByIngredient: (queries) => {
+    spoonacularApp.apiCall(
+      "recipes/findByIngredients",
+      "&ingredients=" + queries, //"pineapple,+flour,+sugar",
+      {
+        method: "GET",
+        "Content-Type": "application/json",
+      }
+    );
+    $("#byIngredientsInput").val("");
+  },
+
+  validateGroceryProduct: () => {
+    var recipeArray = $("#searchGroceryProductInput").val().split(",");
+    var recipeString = recipeArray[0];
+    for (let index = 1; index < recipeArray.length; index++) {
+      recipeString = recipeString + ",+" + recipeArray[index].trim();
+    }
+    //console.log(recipeString);
+    spoonacularApp.searchGroceryProduct(recipeString);
+  },
+
+  // ------nick add nutrients api here------
+  // getNutrients: () => {
+  //   data = spoonacularApp.apiCall();
+  //   spoonacularApp.showNutrients(data);
+  // },
+
+  // showNutrients: () => {
+
+  // },
+
+  searchGroceryProduct: async (queries) => {
+    var data = await spoonacularApp.apiCall(
+      "food/products/search",
+      "&query=" + queries, //?query=pizza
+      {
+        method: "GET",
+        "Content-Type": "application/json",
+      }
+    );
+    $("#searchGroceryProductInput").val("");
+    // If async and await are removed from the code, the function will no longer wait for the response from the API before executing the next line of code. This means that data might not contain the expected result, and the console.log and spoonacularApp.showGroceryProducts statements might produce unexpected results or errors.
+
+    // Instead of using await, you would need to handle the asynchronous nature of the API call using a callback function or by using the .then() method on the returned Promise.
+    spoonacularApp.showGroceryProducts(data);
+  },
+
+  showGroceryProducts: (data) => {
+    var searchContainer = $("#searchResultsContainer");
+    searchContainer.empty();
+    for (let index = 0; index < data.products.length; index += 2) {
+      var image = data.products[index].image;
+      var title = data.products[index].title;
+      var id = data.products[index].id;
+      var image2 = data.products[index + 1].image;
+      var title2 = data.products[index + 1].title;
+      var id2 = data.products[index + 1].id;
+      anchorEl = $("<a>");
+      //anchorEl.css({ display: "block" });
+      //anchorText = anchorEl.text(data.products[index].title);
+      var temp = `
+        <div class="row align-items-center justify-content-center">
+        <div class="card m-2 col-5">
+          <img src="${image}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <a href="./single-item.html?repo=${id}" class="btn btn-primary">Add</a>
+          </div>
+        </div>
+  
+        <div class="card m-2 col-5">
+          <img src="${image2}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${title2}</h5>      
+            <a href="./single-item.html?repo=${id2}" class="btn btn-primary">Add</a>
+          </div>
+        </div>
+        </div>
+        `;
+      searchContainer.append(temp);
+    }
+  },
+};
+
+spoonacularApp.init();
